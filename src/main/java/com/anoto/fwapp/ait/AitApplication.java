0a0ff232084cef0bfd605a183573b21cc64c67a4
Jun Chae <jun.chae@anoto.com>
2021-08-25 15:38:59 +0900

    initial commit

---
+++ src/main/java/com/anoto/fwapp/ait/AitApplication.java
@@ -0,0 +1,13 @@
+package com.anoto.fwapp.ait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anoto/fwapp/ait/AitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anoto.fwapp.ait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
